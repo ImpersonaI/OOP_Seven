bb7fd33825c2dc1b36a7871d3134c9456e44e69b
ImpersonaI <43351355+ImpersonaI@users.noreply.github.com>
2020-12-07 16:12:39 +0300

    Add files via upload

---
+++ OOP_Seven/OOP_Seven/Professor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Seven
+{
+    class Professor
+    {
+   
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+
+        public Professor ()
+        {
+            Console.WriteLine("Input name: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Input Middle name: ");
+            MiddleName = Console.ReadLine();
+            Console.WriteLine("Input Last name: ");
+            LastName = Console.ReadLine();
+            Console.WriteLine("Input age: ");
+            Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input position: ");
+            Position = Console.ReadLine();
+        }
+
+        public Professor(string name, string middlename, string lastname, int age, string position)
+        {
+
+            Name = name;
+            MiddleName = middlename;
+            LastName = lastname;
+            Age = age;
+            Position = position;
+        }
+
+    }
+}
---
+++ OOP_Seven/OOP_Seven/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP_Seven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Move;
+            string Who;
+            int WhoInt;
+
+            List<Professor> Professors = new List<Professor>();
+            Professors.Add(new Professor("1","midname","lastname",44,"Glava"));
+            Professors.Add(new Professor("2", "midname", "lastname", 44, "Glava"));
+            Professors.Add(new Professor("3", "midname", "lastname", 44, "Glava"));
+            Professors.Add(new Professor("4", "midname", "lastname", 44, "Glava"));
+            string name = "3";
+            
+            
+
+            Console.WriteLine("Choose move Add-A, D-Delete, Find-F, Delete them all - Da");
+            Move = Console.ReadLine();
+
+            switch (Move)
+            {
+                case "D":
+
+                    {   
+
+                        Console.WriteLine("Who do you wanna delete?");
+                        WhoInt = Convert.ToInt32(Console.ReadLine());
+                        Professors.RemoveAt(WhoInt);
+                        Console.WriteLine();
+                        break;
+                    }
+                case "F":
+
+                    {
+                        Console.WriteLine("Input name: ");
+                        name = Console.ReadLine();
+
+                        Console.WriteLine("Answer" +
+                        Professors.Find(x => x.Name.Contains(name)));                        
+                        Console.WriteLine();
+                        break;
+                    }
+                case "Da":
+
+                    {
+                        Professors.Clear();
+                        Console.WriteLine("All professors have been deleted");
+                        break;
+                    }                                   
+
+                default:
+                    {  
+
+                        Console.WriteLine("Try again");
+                        break;
+                    }
+                   
+            }
+
+            foreach (var item in Professors)
+            {
+                 
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.LastName);
+                Console.WriteLine(item.MiddleName);
+                Console.WriteLine(item.Age);
+                Console.WriteLine(item.Position);
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
